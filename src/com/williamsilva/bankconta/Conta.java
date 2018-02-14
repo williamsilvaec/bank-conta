32acaa6b0b89aee3a427dbbf2638936c1890fe06
william.silva <william@viasoft.com.br>
2018-02-14 09:59:16 -0200

    Initial commit

---
+++ src/com/williamsilva/bankconta/Conta.java
@@ -0,0 +1,70 @@
+package com.williamsilva.bankconta;
+
+public class Conta {
+
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total = 0;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saca(valor)) {
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+}
---
+++ src/com/williamsilva/bankconta/ContaPoupanca.java
@@ -0,0 +1,8 @@
+package com.williamsilva.bankconta;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int agencia, int numero) {
+        super(agencia, numero);
+    }
+}
